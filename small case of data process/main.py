baa24f15978cd8cf57fe68de67c3c453539ec1df
白时 <zhouyanchen0922@gmail.com>
2026-01-22 17:37:03 +0800

    code
    Read sales data from text and JSON files, merge, and visualize.

--- small case of data process/main.py
+++ small case of data process/main.py
@@ -1,1 +1,39 @@
 
+from file_define import TextFileReader, JsonFileReader
+from data_define import Record
+from pyecharts.charts import Bar
+from pyecharts.options import *
+from pyecharts.globals import ThemeType
+
+
+text_file_reader = TextFileReader("D:/2011年1月销售数据.txt")
+json_file_reader = JsonFileReader("D:/2011年2月销售数据JSON.txt")
+
+jan_data: list[Record] = text_file_reader.read_data()
+feb_data: list[Record] = json_file_reader.read_data()
+# 将2个月份的数据合并为1个list来存储
+all_data: list[Record] = jan_data + feb_data
+
+data_dict = {}
+for record in all_data:
+    if record.date in data_dict.keys():
+        data_dict[record.date]+=record.money
+    else:
+        data_dict[record.date]=record.money
+
+
+print(data_dict)
+
+#可视化的图表开发
+bar = Bar(init_opts=InitOpts(theme=ThemeType.LIGHT))
+
+bar.add_xaxis(list(data_dict.keys()))
+bar.add_yaxis("销售额",list(data_dict.values()),label_opts=LabelOpts(is_show=False))
+bar.set_global_opts(
+    title_opts=TitleOpts(title="每日销售额")
+)
+
+bar.render("每日销售额柱状图.html")
+
+
+
